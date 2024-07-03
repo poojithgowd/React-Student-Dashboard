6531226da55e89bd5054fbed23a3070144298bf2
poojithgowd <poojith29gowd@gmail.com>
2024-07-03 14:39:35 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { useState } from 'react'
+import Styles from './Components/Styles.css'
+import learn from "../src/assets/learn.png"
+import peer from "../src/assets/peer.png"
+import settings from "../src/assets/settings.png"
+import Card2 from './Components/Card2'
+import student from './assets/student (1).png'
+import star from "./assets/starlogo.png"
+import cup from './assets/TpCYeB-winner-clipart-hd-logo.png'
+import flag from '../src/assets/Flag-Australia.webp'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid bg p-2' >
+   <div className='border m-3 rounded-4 d-flex abc'>
+    <div className='cards1 text-center' style={{color:"white", width:"12%",height:"90vh", backgroundColor:'rgba(255, 255, 255, 0.05)'}} >
+      <h3 className='p-4'><strong>DEL</strong></h3>
+      <div><a href=""><img src={learn} alt="no image" width={"30%"} className='mx-5'/></a></div>
+      <p className='px-4'>MY learning plan</p>
+      
+      <div><a href=""><img src={peer} alt="no" width={"30%"} className='mx-5 my-3'/></a></div>
+      <p className='px-4'>MY peers</p>
+      <div><a href=""><img src={settings} alt="no" width={"30%"} className='mx-5 my-3'/></a></div>
+      <p className='px-4'>Settings</p>
+      <div  className='px-5 ' style={{marginTop:"80px"}}><h6>Nikitin</h6></div>
+      <div><img src={student} alt=""  style={{width:"30px",height:"30px",backgroundColor:"yellow"}} className='border m-1'/>
+      <img src={flag} alt="" style={{width:"30px",height:"30px"}} className='border ' />
+      </div>
+
+
     </div>
-  );
+   <Card2/>
+
+   
+    <div className='card3 overflow-hidden ' style={{color:"white", width:"30%",height:"90vh",backgroundColor:"#FECD09" ,borderRadius:"0px 15px 15px"}}  >
+    <img src={cup} alt="" style={{width:"80px",height:"50px",position:'absolute',marginLeft:"-40px", marginTop:"30px"}} />
+
+      <h3 className='p-4 ms-5' style={{color:"black"}}><strong>Leader-Board</strong></h3>
+
+      <div  >
+        <div className='d-flex justify-content-around' style={{color:"black"}}>
+       <div className='pt-4'>90%</div>
+       <div>100%</div>
+       <div className='pt-4'>80%</div>
+       </div>
+      <div className='d-flex' >
+         <div className=' mt-4' style={{width:"120px",backgroundColor:"#3b3461",borderRadius:"40px 40px 0px 0px"}}>
+         <div className='d-flex justify-content-center p-1'>
+          <img src={star} alt="" style={{width:"40px", height:"40px",position:'absolute'}}  /> <div className='p-2 '>2</div></div>
+          <div><img src={student} alt="noimage" style={{width:"75px"}} className='rounded-4 ms-4'/></div>
+          <div className='d-flex justify-content-center'>Koresh</div>
+          <div></div>
+          <div></div>
+
+         </div>
+         <div className='  ' style={{width:"120px",backgroundColor:"#3b3461" ,borderRadius:"40px 40px 0px 0px"}}>
+         <div className='d-flex justify-content-center p-1'>
+          <img src={star} alt="" style={{width:"40px", height:"40px",position:'absolute'}}  /> <div className='p-2 '>1</div></div>
+          <div><img src={student} alt="noimage" style={{width:"75px"}} className='rounded-4 ms-4'/></div>
+          <div className='d-flex justify-content-center'>Sai</div>
+          <div></div>
+          <div></div>
+
+         </div>
+         <div className='   mt-5' style={{width:"120px",backgroundColor:"#3b3461",borderRadius:"40px 40px 0px 0px"}}>
+         <div className='d-flex justify-content-center p-1'>
+          <img src={star} alt="" style={{width:"40px", height:"40px",position:'absolute'}}  /> <div className='p-2 '>3</div></div>
+          <div><img src={student} alt="noimage" style={{width:"75px"}} className='rounded-4 ms-4'/></div>
+          <div className='d-flex justify-content-center'>Vinay</div>
+          <div></div>
+          <div></div>
+
+         </div>
+         </div>
+    <div className=' pt-2 d-flex ' style={{backgroundColor:"#3b3461"}}>
+      <div className='ps-5' >
+      <img src={star} alt="" style={{width:"40px", height:"40px",position:'absolute'}} className='' /> <div className='p-2 ps-3'>4</div>
+      </div>
+      <div className='ps-4 pt-2'>Vijay</div>
+      <div className='ms-auto pe-5 pt-2'>70%</div>
+    </div>
+    <div className=' pt-3 d-flex' style={{backgroundColor:"#3b3461"}}>
+      <div className='ps-5' >
+      <img src={star} alt="" style={{width:"40px", height:"40px",position:'absolute'}} className='' /> <div className='p-2 ps-3'>5</div>
+      </div>
+      <div className='ps-4 pt-2'>Sonia</div>
+      <div className='ms-auto pe-5 pt-2'>60%</div>
+    </div> <div className=' pt-4 d-flex' style={{backgroundColor:"#3b3461"}}>
+      <div className='ps-5' >
+      <img src={star} alt="" style={{width:"40px", height:"40px",position:'absolute'}} className='' /> <div className='p-2 ps-3'>6</div>
+      </div>
+      <div className='ps-4 pt-2'>Harsha</div>
+      <div className='ms-auto pe-5 pt-2'>50%</div>
+    </div> <div className=' pt-3 d-flex' style={{backgroundColor:"#3b3461"}}>
+      <div className='ps-5' >
+      <img src={star} alt="" style={{width:"40px", height:"40px",position:'absolute'}} className='' /> <div className='p-2 ps-3'>7</div>
+      </div>
+      <div className='ps-4 pt-2'>Vivek</div>
+      <div className='ms-auto pe-5 pt-2'>40%</div>
+    </div>
+    <div className=' pt-3 d-flex' style={{backgroundColor:"#3b3461"}}>
+      <div className='ps-5' >
+      <img src={star} alt="" style={{width:"40px", height:"40px",position:'absolute'}} className='' /> <div className='p-2 ps-3'>8</div>
+      </div>
+      <div className='ps-4 pt-2'>Lilly</div>
+      <div className='ms-auto pe-5 pt-2'>30%</div>
+    </div>
+    <div className=' pt-3 d-flex' style={{backgroundColor:"#3b3461",borderRadius:"0px 0px 2px"}}>
+      <div className='ps-5' >
+      <img src={star} alt="" style={{width:"40px", height:"40px",position:'absolute'}} className='' /> <div className='p-2 ps-3'>9</div>
+      </div>
+      <div className='ps-4 pt-2'>Lovely</div>
+      <div className='ms-auto pe-5 pt-2'>35%</div>
+    </div>
+      
+    </div>
+   
+
+     
+
+
+    </div>
+         
+       
+   
+        </div>
+        </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Card2.js
@@ -0,0 +1,271 @@
+import React from 'react'
+import stud1 from"../assets/student (1).png"
+import loc from "../assets/location.png"
+import std from "../assets/student (2).png"
+const Card2 = () => {
+  const handleMouseEnter = (event) => {
+    event.currentTarget.style.transform = 'rotate(-10deg)scale(1.09)';
+    
+  };
+
+  const handleMouseLeave = (event) => {
+    event.currentTarget.style.transform = 'rotate(0deg)';
+  };
+  return (
+    <div className='cards2 text-center overflow-hidden ' style={{color:"white", width:"76%",height:"90vh"}}  >
+    <h3 className='p-4 '><strong>My Peers</strong><span className='' style={{marginLeft:"250px"}}><input type="text" placeholder='🔍 Search' style={{backgroundColor:'#352d5c'}}/></span></h3>
+    <div className='d-flex flex-wrap mt-3
+    ' style={{marginLeft:"100px"}}>
+      <div><div className='border rounded-4  pt-3 m-3' style={{width:"200px",backgroundColor:'rgba(255, 255, 255, 0.1)',transition: 'transform 0.2s' }}
+     onMouseEnter={handleMouseEnter}
+     onMouseLeave={handleMouseLeave}>
+      <div className='d-flex justify-content-center '>
+        <div><img src={stud1} alt="noimage" style={{width:"75px" , backgroundColor:"yellow"}} className='rounded-4 ms-4'/></div>
+        <spam className='ps-1'>
+          <h5 style={{color:'pink'}}><strong>19</strong></h5>
+          <h5>Mar</h5>
+        </spam>
+        
+      </div>
+      <h5 className='pt-2'><b>Sreenu</b> </h5>
+      <h6  style={{color:"orange"}}>Sales Representative</h6>
+      <div className='d-flex justify-content-center'>
+        <img src={loc} alt=""  style={{width:"25px", height:"20px"}}/><h6 className=''>Berlin,</h6>
+
+      </div>
+      <p>Germany-Midwest</p>
+     <div className='container  '>
+     <div class="progress" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar bg-warning text-dark" style={{width: "70%"}}>1.lesson-ABC</div>
+</div>
+    
+            
+
+     </div>
+        
+    </div></div>
+    
+    <div className='border rounded-4  pt-3 m-3' style={{width:"200px",backgroundColor:'rgba(255, 255, 255, 0.1)',transition: 'transform 0.2s' }} onMouseEnter={handleMouseEnter}
+     onMouseLeave={handleMouseLeave}>
+      <div className='d-flex justify-content-center '>
+        <div><img src={stud1} alt="noimage" style={{width:"75px" , backgroundColor:"yellow"}} className='rounded-4 ms-4'/></div>
+        <spam className='ps-1'>
+          <h5 style={{color:'pink'}}><strong>20</strong></h5>
+          <h5>Apr</h5>
+        </spam>
+        
+      </div>
+      <h5 className='pt-2'><b>Sanjay</b> </h5>
+      <h6  style={{color:"orange"}}>Ceo</h6>
+      <div className='d-flex justify-content-center'>
+        <img src={loc} alt=""  style={{width:"25px", height:"20px"}}/><h6 className=''>Berlin,</h6>
+
+      </div>
+      <p>Amalapuram</p>
+     <div className='container  '>
+     <div class="progress" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar bg-warning text-dark" style={{width: "90%"}}>2.lesson-ABC</div>
+</div>
+    
+            
+
+     </div>
+        
+    </div>
+    <div className='border rounded-4  pt-3 m-3' style={{width:"200px",backgroundColor:'rgba(255, 255, 255, 0.1)',transition: 'transform 0.2s' }} onMouseEnter={handleMouseEnter}
+     onMouseLeave={handleMouseLeave}>
+      <div className='d-flex justify-content-center '>
+        <div><img src={std} alt="noimage" style={{width:"75px" , backgroundColor:"yellow"}} className='rounded-4 ms-4'/></div>
+        <spam className='ps-1'>
+          <h5 style={{color:'pink'}}><strong>30</strong></h5>
+          <h5>Nov</h5>
+        </spam>
+        
+      </div>
+      <h5 className='pt-2'><b>Fareeza</b> </h5>
+      <h6  style={{color:"orange"}}>Manager</h6>
+      <div className='d-flex justify-content-center'>
+        <img src={loc} alt=""  style={{width:"25px", height:"20px"}}/><h6 className=''>Berlin,</h6>
+
+      </div>
+      <p>Germany-Midwest</p>
+     <div className='container  '>
+     <div class="progress" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar bg-warning text-dark" style={{width: "70%"}}>1.lesson-ABC</div>
+</div>
+    
+            
+
+     </div>
+        
+    </div>
+    <div className='border rounded-4  pt-3 m-3' style={{width:"200px",backgroundColor:'rgba(255, 255, 255, 0.1)',transition: 'transform 0.2s' }} onMouseEnter={handleMouseEnter}
+     onMouseLeave={handleMouseLeave}>
+      <div className='d-flex justify-content-center '>
+        <div><img src={stud1} alt="noimage" style={{width:"75px" , backgroundColor:"yellow"}} className='rounded-4 ms-4'/></div>
+        <spam className='ps-1'>
+          <h5 style={{color:'pink'}}><strong>19</strong></h5>
+          <h5>Mar</h5>
+        </spam>
+        
+      </div>
+      <h5 className='pt-2'><b>Sanju</b> </h5>
+      <h6  style={{color:"orange"}}>Sales Representative</h6>
+      <div className='d-flex justify-content-center'>
+        <img src={loc} alt=""  style={{width:"25px", height:"20px"}}/><h6 className=''>Berlin,</h6>
+
+      </div>
+      <p>Germany-Midwest</p>
+     <div className='container  '>
+     <div class="progress" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar bg-warning text-dark" style={{width: "70%"}}>1.lesson-ABC</div>
+</div>
+    
+            
+
+     </div>
+        
+    </div>
+    <div className='border rounded-4  pt-3 m-3' style={{width:"200px",backgroundColor:'rgba(255, 255, 255, 0.1)',transition: 'transform 0.2s' }} onMouseEnter={handleMouseEnter}
+     onMouseLeave={handleMouseLeave}>
+      <div className='d-flex justify-content-center '>
+        <div><img src={stud1} alt="noimage" style={{width:"75px" , backgroundColor:"yellow"}} className='rounded-4 ms-4'/></div>
+        <spam className='ps-1'>
+          <h5 style={{color:'pink'}}><strong>19</strong></h5>
+          <h5>Mar</h5>
+        </spam>
+        
+      </div>
+      <h5 className='pt-2'><b>Sujith</b> </h5>
+      <h6  style={{color:"orange"}}>Sales Representative</h6>
+      <div className='d-flex justify-content-center'>
+        <img src={loc} alt=""  style={{width:"25px", height:"20px"}}/><h6 className=''>Berlin,</h6>
+
+      </div>
+      <p>Germany-Midwest</p>
+     <div className='container  '>
+     <div class="progress" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar bg-warning text-dark" style={{width: "70%"}}>1.lesson-ABC</div>
+</div>
+    
+            
+
+     </div>
+        
+    </div>
+    <div className='border rounded-4  pt-3 m-3' style={{width:"200px",backgroundColor:'rgba(255, 255, 255, 0.1)',transition: 'transform 0.2s' }} onMouseEnter={handleMouseEnter}
+     onMouseLeave={handleMouseLeave}>
+      <div className='d-flex justify-content-center '>
+        <div><img src={stud1} alt="noimage" style={{width:"75px" , backgroundColor:"yellow"}} className='rounded-4 ms-4'/></div>
+        <spam className='ps-1'>
+          <h5 style={{color:'pink'}}><strong>19</strong></h5>
+          <h5>Mar</h5>
+        </spam>
+        
+      </div>
+      <h5 className='pt-2'><b>Michael Kopfler</b> </h5>
+      <h6  style={{color:"orange"}}>Sales Representative</h6>
+      <div className='d-flex justify-content-center'>
+        <img src={loc} alt=""  style={{width:"25px", height:"20px"}}/><h6 className=''>Berlin,</h6>
+
+      </div>
+      <p>Germany-Midwest</p>
+     <div className='container  '>
+     <div class="progress" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar bg-warning text-dark" style={{width: "70%"}}>1.lesson-ABC</div>
+</div>
+    
+            
+
+     </div>
+        
+    </div>
+    <div className='border rounded-4  pt-3 m-3' style={{width:"200px",backgroundColor:'rgba(255, 255, 255, 0.1)',transition: 'transform 0.2s' }} onMouseEnter={handleMouseEnter}
+     onMouseLeave={handleMouseLeave}>
+      <div className='d-flex justify-content-center '>
+        <div><img src={stud1} alt="noimage" style={{width:"75px" , backgroundColor:"yellow"}} className='rounded-4 ms-4'/></div>
+        <spam className='ps-1'>
+          <h5 style={{color:'pink'}}><strong>19</strong></h5>
+          <h5>Mar</h5>
+        </spam>
+        
+      </div>
+      <h5 className='pt-2'><b>Michael Kopfler</b> </h5>
+      <h6  style={{color:"orange"}}>Sales Representative</h6>
+      <div className='d-flex justify-content-center'>
+        <img src={loc} alt=""  style={{width:"25px", height:"20px"}}/><h6 className=''>Berlin,</h6>
+
+      </div>
+      <p>Germany-Midwest</p>
+     <div className='container  '>
+     <div class="progress" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar bg-warning text-dark" style={{width: "70%"}}>1.lesson-ABC</div>
+</div>
+    
+            
+
+     </div>
+        
+    </div>
+    <div className='border rounded-4  pt-3 m-3' style={{width:"200px",backgroundColor:'rgba(255, 255, 255, 0.1)',transition: 'transform 0.2s' }} onMouseEnter={handleMouseEnter}
+     onMouseLeave={handleMouseLeave}>
+      <div className='d-flex justify-content-center '>
+        <div><img src={stud1} alt="noimage" style={{width:"75px" , backgroundColor:"yellow"}} className='rounded-4 ms-4'/></div>
+        <spam className='ps-1'>
+          <h5 style={{color:'pink'}}><strong>19</strong></h5>
+          <h5>Mar</h5>
+        </spam>
+        
+      </div>
+      <h5 className='pt-2'><b>Michael Kopfler</b> </h5>
+      <h6  style={{color:"orange"}}>Sales Representative</h6>
+      <div className='d-flex justify-content-center'>
+        <img src={loc} alt=""  style={{width:"25px", height:"20px"}}/><h6 className=''>Berlin,</h6>
+
+      </div>
+      <p>Germany-Midwest</p>
+     <div className='container  '>
+     <div class="progress" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar bg-warning text-dark" style={{width: "70%"}}>1.lesson-ABC</div>
+</div>
+    
+            
+
+     </div>
+        
+    </div>
+    <div className='border rounded-4  pt-3 m-3' style={{width:"200px",backgroundColor:'rgba(255, 255, 255, 0.1)',transition: 'transform 0.2s' }} onMouseEnter={handleMouseEnter}
+     onMouseLeave={handleMouseLeave}>
+      <div className='d-flex justify-content-center '>
+        <div><img src={stud1} alt="noimage" style={{width:"75px" , backgroundColor:"yellow"}} className='rounded-4 ms-4'/></div>
+        <spam className='ps-1'>
+          <h5 style={{color:'pink'}}><strong>19</strong></h5>
+          <h5>Mar</h5>
+        </spam>
+        
+      </div>
+      <h5 className='pt-2'><b>Michael Kopfler</b> </h5>
+      <h6  style={{color:"orange"}}>Sales Representative</h6>
+      <div className='d-flex justify-content-center'>
+        <img src={loc} alt=""  style={{width:"25px", height:"20px"}}/><h6 className=''>Berlin,</h6>
+
+      </div>
+      <p>Germany-Midwest</p>
+     <div className='container  '>
+     <div class="progress" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar bg-warning text-dark" style={{width: "70%"}}>1.lesson-ABC</div>
+</div>
+    
+            
+
+     </div>
+        
+    </div>
+    </div>
+    
+
+  </div>
+  )
+}
+
+export default Card2
